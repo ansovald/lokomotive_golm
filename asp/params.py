2af542d12498d9b90536da757d54ffee7c3b40e4
TimLeiber <tim.leiber@uni-potsdam.de>
2025-11-16 11:58:58 +0100

    add flatland toolkit to repo. fix bug in solve.py related to action_map. move encodings into asp directory

---
+++ asp/params.py
@@ -0,0 +1,11 @@
+#primary=['asp/graph_based/actions.lp','asp/graph_based/graph.lp','asp/graph_based/traverse.lp']
+#primary=['asp/action_based/actions.lp','asp/action_based/pathfinding.lp','asp/action_based/transitions.lp']
+
+# karl encoding
+#primary=[]
+
+# tim encoding
+#primary=['asp/']
+primary=['asp/assignment_1_tim/encoding.lp', 'asp/assignment_1_tim/tracks.lp']
+
+secondary=[]
---
+++ envs/params.py
@@ -0,0 +1,23 @@
+# basic parameters
+width=30
+height=30
+number_of_agents=2
+max_num_cities=2
+seed=1
+grid_mode=False
+max_rails_between_cities=2
+max_rail_pairs_in_city=2
+remove_agents_at_target=True
+
+# speed
+speed_ratio_map={1 : 1}
+"""speed_ratio_map= {
+        1:   1,
+        1/2: 0.00,
+        1/3: 0.00,
+        1/4: 0.00
+    }"""
+# malfunctions
+malfunction_rate=0/30
+min_duration=2
+max_duration=6
