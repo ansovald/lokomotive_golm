b13090945e32a55ea36fbdd0eebe45465e1f6c61
Karl Osswald <ansovald@posteo.de>
2025-11-30 15:40:49 +0100

    slow speed exp

--- envs/params.py
+++ envs/params.py
@@ -1,8 +1,8 @@
 # basic parameters
 width=40
 height=40
-number_of_agents=6
-max_num_cities=4
+number_of_agents=3
+max_num_cities=3
 seed=1
 grid_mode=False
 max_rails_between_cities=2
@@ -10,7 +10,11 @@ max_rail_pairs_in_city=2
 remove_agents_at_target=True
 
 # speed
-speed_ratio_map={1 : 1}
+speed_ratio_map={1 : 0.4,
+                1/2 : 0.3,
+                1/3 : 0.3,
+                1/4 : 0.0
+                }
 """speed_ratio_map= {
         1:   1,
         1/2: 0.00,
